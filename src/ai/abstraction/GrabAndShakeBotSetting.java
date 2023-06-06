7bf4abfaaf9f4ff0fb0b577006607dfdafc5635e
Adrian Bremer <adrian.bremer@st.ovu.de>
2023-06-06 15:44:31 +0200

    first version with using WorkerRushPlusPlus and CRush_V2 done

---
+++ src/ai/abstraction/GrabAndShakeBotSetting.java
@@ -0,0 +1,21 @@
+package ai.abstraction;
+
+import ai.core.AIWithComputationBudget;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrabAndShakeBotSetting {
+    List<Rectangle> territories = new ArrayList<>();
+    List<AIWithComputationBudget> usedBots = new ArrayList<>();
+
+    public void parseFromFile(String file) {
+        // TODO: parse
+    }
+
+    public String toString() {
+        // TODO: tostring
+        return "GrabAndShakeBotSetting";
+    }
+}
